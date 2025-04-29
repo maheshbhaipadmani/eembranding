b45e941f35ac02920553de7c92969f3def0e4aa4
Brijesh Dhorajiya <brijesh.dhorajiya@uniqueconsumerservices.com>
2025-04-29 12:08:44 +0530

    fix the bug in blog page

--- blogs.php
+++ blogs.php
@@ -86,7 +86,7 @@ while ($item = current($myaraa)) {
         <div class="rt-holder tpg-post-holder">
             <div class="rt-detail rt-el-content-wrapper">
                 <div class="rt-img-holder tpg-el-image-wrap has-thumbnail">
-                    <a data-id="7100" href="/eembranding/<?php echo $item["URL"]; ?>" class="tpg-post-link" target="_self">
+                    <a data-id="7100" href="<?php echo $item["URL"]; ?>" class="tpg-post-link" target="_self">
                     <img fetchpriority="high" decoding="async" src="<?php echo $item['Thumbnail']; ?>"
                              class="rt-img-responsive" width="1200" height="800"
                              alt="The Power of Graphic Design in Brand Identity Building">
@@ -101,7 +101,7 @@ while ($item = current($myaraa)) {
                         </span>
                     </div>
                     <h3 class="entry-title">
-                        <a data-id="7100" href="/eembranding/<?php echo $item["URL"]; ?>" class="tpg-post-link" target="_self">
+                        <a data-id="7100" href="<?php echo $item["URL"]; ?>" class="tpg-post-link" target="_self">
                         <?php echo $item["BlogTitle"]; ?>
 
                         </a>
@@ -129,7 +129,7 @@ while ($item = current($myaraa)) {
 
                 <div class="post-footer">
                     <div class="read-more">
-                        <a data-id="7100" href="/eembranding/<?php echo $item["URL"]; ?>" class="tpg-post-link" target="_self">Read More...</a>
+                        <a data-id="7100" href="<?php echo $item["URL"]; ?>" class="tpg-post-link" target="_self">Read More...</a>
                     </div>
                 </div>
             </div>
