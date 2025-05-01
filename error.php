585785e2840e58b7db063151214e7a1d8b30d37c
hitixa-bhuva <hitixa.bhuva@theuniqueitsolution.com>
2025-05-01 12:10:26 +0530

    test url check

---
+++ error.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404</title>
+    <style>
+        body {
+	height: 100vh;
+	background: radial-gradient(black, transparent);
+	display: grid;
+  font-family: 'Roboto Condensed', sans-serif;
+}
+  p, a {
+    margin: 0;
+    position: absolute;
+    text-decoration: none;
+    text-transform: uppercase;
+    color: #032C3F;
+    font-size: 24px;
+  }
+  .textA {
+    top: 169px;
+    left: 257px;
+    transform: rotate(-3deg);
+  }
+  .textB {
+    font-weight: 700;
+    font-size: 135px;
+    top: 209px;
+    left: 273px;
+    transform: rotate(-2deg);
+  }
+  .textC {
+    top: 370px;
+    left: 428px;
+    font-size: 30px;
+    transform: rotate(1deg);
+  }
+.container {
+  position: relative;
+  height: 600px;
+  width: 750px;
+  margin: auto;
+}
+
+.container svg {
+  height: 100%;
+}
+
+    </style>
+</head>
+<body>
+    <div class="container">
+      <svg class="page-not-found" viewBox="0 0 1280 1024">
+              <title>Page Not Found</title>
+                <g class="spin-circles hide">
+                      <path d="M776.3,403.3A151,151,0,0,1,989.8,189.8" style="fill: none;stroke: #de202a;stroke-miterlimit: 10;stroke-width: 8px"/>
+                      <path d="M989.8,189.8A151,151,0,0,1,776.3,403.3" style="fill: none;stroke: #fff;stroke-miterlimit: 10;stroke-width: 2px"/>
+                      <path d="M786.9,392.7c-53-53-52.8-138.9.3-192.1s139.1-53.2,192.1-.3" style="fill: none;stroke: #de202a;stroke-miterlimit: 10;stroke-width: 8px"/>
+                      <path d="M979.2,200.4c53,53,52.8,138.9-.3,192.1s-139.1,53.2-192.1.3" style="fill: none;stroke: #fff;stroke-miterlimit: 10;stroke-width: 2px"/>
+                      <path d="M797.5,382.1c-46.9-46.9-46.7-123.3.6-170.6s123.6-47.5,170.6-.6" style="fill: none;stroke: #de202a;stroke-miterlimit: 10;stroke-width: 8px"/>
+                      <path d="M968.6,211c46.9,46.9,46.7,123.3-.6,170.6s-123.6,47.5-170.6.6" style="fill: none;stroke: #fff;stroke-miterlimit: 10;stroke-width: 2px"/>
+                      <path d="M808.1,371.5c-40.9-40.9-40.6-107.7.8-149.1s108.1-41.8,149.1-.8" style="fill: none;stroke: #de202a;stroke-miterlimit: 10;stroke-width: 8px"/>
+                      <path d="M958,221.6c40.9,40.9,40.6,107.7-.8,149.1s-108.1,41.8-149.1.8" style="fill: none;stroke: #fff;stroke-miterlimit: 10;stroke-width: 2px"/>
+                      <path d="M818.7,360.9c-34.9-34.9-34.4-92,1.1-127.6s92.7-36,127.6-1.1" style="fill: none;stroke: #de202a;stroke-miterlimit: 10;stroke-width: 8px"/>
+                      <path d="M947.4,232.2c34.9,34.9,34.4,92-1.1,127.6s-92.7,36-127.6,1.1" style="fill: none;stroke: #fff;stroke-miterlimit: 10;stroke-width: 2px"/>
+                      <path d="M829.3,350.3c-28.9-28.9-28.3-76.4,1.4-106.1s77.2-30.3,106.1-1.4" style="fill: none;stroke: #de202a;stroke-miterlimit: 10;stroke-width: 8px"/>
+                      <path d="M936.8,242.8c28.9,28.9,28.3,76.4-1.4,106.1s-77.2,30.3-106.1,1.4" style="fill: none;stroke: #fff;stroke-miterlimit: 10;stroke-width: 2px"/>
+                </g>
+                <g class="big-white-circle hide">
+                    <circle cx="644" cy="482" r="277" style="fill: #fff"/>
+                </g>
+                <g class="grow-circles hide">
+                      <circle cx="940" cy="523" r="27" style="fill: #ffe029"/>
+                      <circle cx="708.5" cy="217.6" r="14.5" transform="translate(-21.2 87.8) rotate(-7)" style="fill: #00b3ac"/>
+                      <circle cx="725.5" cy="755.6" r="14.5" transform="translate(-86.5 93.8) rotate(-7)" style="fill: #f62c72"/>
+                      <circle cx="446.9" cy="181.6" r="16.5" transform="translate(-18.8 55.7) rotate(-7)" style="fill: #00b3ac"/>
+                      <circle cx="314.9" cy="348.6" r="16.5" transform="translate(-40.1 40.9) rotate(-7)" style="fill: #f62c72"/>
+                      <circle cx="682.6" cy="187.5" r="7.5" transform="translate(-17.7 84.4) rotate(-7)" style="fill: #f62c72"/>
+                      <circle cx="328.5" cy="500.6" r="7.5" transform="translate(-58.4 43.7) rotate(-7)" style="fill: #fff"/>
+                      <circle cx="364" cy="439" r="27" style="fill: #ffe029"/>
+                </g>
+                <g class="box">
+                      <polygon class="main-container" points="364.4 328.1 888.1 292.8 889.1 635 453.7 698.2 364.4 328.1" style="fill: #fff;stroke: #032c3f;stroke-miterlimit: 10;stroke-width: 24px"/>
+                      <polygon points="395 285.6 762 258.9 732.7 336.1 431.6 350.2 395 285.6" style="fill: #ffe029"/>
+                      <polygon points="709.7 622 933.7 622.3 960.9 662.9 931.9 700.1 708.7 699.4 709.7 622" style="fill: #ffe029"/>
+                </g>
+                <g class="squiggles hide">
+                 
+                </g>
+                <g class="words hide">
+                  <text transform="translate(471.5 329.1) rotate(-2.9)" style="font-size: 40px;fill: #032c3f;font-family: Futura-CondensedMedium, Futura">P</text>
+                  <text transform="translate(486.4 328.4) rotate(-2.9)" style="font-size: 40px;fill: #032c3f;font-family: Futura-CondensedMedium, Futura">A</text>
+                  <text transform="translate(502.6 327.5) rotate(-2.9)" style="font-size: 40px;fill: #032c3f;font-family: Futura-CondensedMedium, Futura">G</text>
+                  <text transform="matrix(1, -0.05, 0.05, 1, 521.22, 326.59)" style="font-size: 40px;fill: #032c3f;font-family: Futura-CondensedMedium, Futura">E</text>
+                  <text transform="matrix(1, -0.05, 0.05, 1, 535.73, 325.85)" style="font-size: 40px;fill: #032c3f;font-family: Futura-CondensedMedium, Futura"> </text>
+                  <text transform="matrix(1, -0.05, 0.05, 1, 544, 325.43)" style="font-size: 40px;fill: #032c3f;font-family: Futura-CondensedMedium, Futura">N</text>
+                  <text transform="translate(562.7 324.5) rotate(-2.9)" style="font-size: 40px;fill: #032c3f;font-family: Futura-CondensedMedium, Futura">O</text>
+                  <text transform="translate(582.1 323.5) rotate(-2.9)" style="font-size: 40px;fill: #032c3f;font-family: Futura-CondensedMedium, Futura">T</text>
+                  <text transform="matrix(1, -0.05, 0.05, 1, 595.15, 322.83)" style="font-size: 40px;fill: #032c3f;font-family: Futura-CondensedMedium, Futura"> </text>
+                  <text transform="translate(603.4 322.4) rotate(-2.9)" style="font-size: 40px;fill: #032c3f;font-family: Futura-CondensedMedium, Futura">F</text>
+                  <text transform="translate(617.5 321.7) rotate(-2.9)" style="font-size: 40px;fill: #032c3f;font-family: Futura-CondensedMedium, Futura">O</text>
+                  <text transform="translate(636.6 320.7) rotate(-2.9)" style="font-size: 40px;fill: #032c3f;font-family: Futura-CondensedMedium, Futura">U</text>
+                  <text transform="translate(655.1 319.8) rotate(-2.9)" style="font-size: 40px;fill: #032c3f;font-family: Futura-CondensedMedium, Futura">N</text>
+                  <text transform="matrix(1, -0.05, 0.05, 1, 673.8, 318.83)" style="font-size: 40px;fill: #032c3f;font-family: Futura-CondensedMedium, Futura">D</text>
+                  <text transform="translate(742 682.9)" style="font-size: 60px;fill: #032c3f;font-family: Futura-CondensedMedium, Futura">GO <tspan x="69.1" y="0" style="letter-spacing: 0.0029296875em">B</tspan><tspan x="94" y="0" style="letter-spacing: -0.0068359375em">A</tspan><tspan x="118" y="0">CK</tspan></text>
+                  <text transform="translate(479.2 549.5)" style="font-size: 180px;fill: #032c3f;font-family: Futura-CondensedExtraBold, Futura;font-weight: 700">4<tspan x="108.3" y="0" style="letter-spacing: -0.0009765625em">0</tspan><tspan x="216.4" y="0">4</tspan></text>
+                </g>
+              </svg>
+    </div>
+
+</body>
+</html>
