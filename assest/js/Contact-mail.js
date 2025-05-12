6d7081a997fe8c409e2fb6c14d74df3afa6a99ac
hitixa-bhuva <hitixa.bhuva@theuniqueitsolution.com>
2025-05-12 10:24:42 +0530

    ReciverEmail change

--- assest/js/Contact-mail.js
+++ assest/js/Contact-mail.js
@@ -22,7 +22,7 @@ console.log("Collected form data:", { name, email, number, subject, message });
         "SMTPSecure": "ssl",
         "SenderEmail": "hitixa.bhuva@uniqueconsumerservices.com",
         "SenderEmailPassword": "1f1UOc{3U*64",
-        "ReciverEmail": "patelhitixa4439@gmail.com",
+        "ReciverEmail": "eembranding@gmail.com",
         "Subject": subject,
         "Body": `
    <!DOCTYPE html>
