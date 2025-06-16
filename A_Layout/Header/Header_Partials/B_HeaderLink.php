5a3821d5a10506cec3693365ffc4590cbca7d021
hitixa-bhuva <hitixa.bhuva@theuniqueitsolution.com>
2025-06-16 10:32:02 +0530

    test

--- A_Layout/Header/Header_Partials/B_HeaderLink.php
+++ A_Layout/Header/Header_Partials/B_HeaderLink.php
@@ -23,7 +23,18 @@ echo
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" id="min-header-0-css"  href="../../../assest/css/minify-header-1986386b.min.css"  type="text/css" media="all">
      <!-- Google Tag Manager -->
-
+    <script>
+        (function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({\'gtm.start\': new Date().getTime(), event:\'gtm.js\'});
+            var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),
+                dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';
+            j.async=true;
+            j.src=\'https://www.googletagmanager.com/gtm.js?id=\'+i+dl;
+            f.parentNode.insertBefore(j,f);
+        })(window,document,\'script\',\'dataLayer\',\'GTM-TRZNTVHB\');
+    </script>
     </head>
 '
 ?>
