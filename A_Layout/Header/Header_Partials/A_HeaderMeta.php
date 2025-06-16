e89846c4d7432b85e811d06e1bb7a7bad6a72367
hitixa-bhuva <hitixa.bhuva@theuniqueitsolution.com>
2025-06-16 11:08:03 +0530

    test base url

--- A_Layout/Header/Header_Partials/A_HeaderMeta.php
+++ A_Layout/Header/Header_Partials/A_HeaderMeta.php
@@ -5,7 +5,8 @@ echo
 <html lang="en">
 
 <head>
-<base href="https://eembranding.com/" />
+<base href="https://eembranding.com/">
+
 <meta charset="utf-8" />
 <meta http-equiv="x-ua-compatible" content="ie=edge" />
 
