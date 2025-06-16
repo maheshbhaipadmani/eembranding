af92706e341b279d8bc8447f7c13539ba95f5b1c
hitixa-bhuva <hitixa.bhuva@theuniqueitsolution.com>
2025-06-16 10:25:24 +0530

    test in js file

--- A_Layout/Footer/Footer_Partials/B_FooterScript.php
+++ A_Layout/Footer/Footer_Partials/B_FooterScript.php
@@ -4,18 +4,18 @@ echo
     <!-- Javascript Files
     ================================================== -->
 
-    <script src="./assest/js/portfolio.js"></script>
+    <script src="../../../assest/js/portfolio.js"></script>
 
     <!-- Slick Slider -->
     <script src="https://cdn.jsdelivr.net/npm/slick-carousel/slick/slick.min.js"></script>
 
     <!-- swiper -->
-     <script src="./assest/js/slider.js"></script>
-    <script src="./assest/js/nav.js"></script>
-     <script src="./assest/js/swiper.min.js"></script>
-     <script src="./assest/js/whatsapp.js"></script>
-    <script type="text/javascript" src="./assest/js/minify-footer-2ef81eaa.min.js"  id="min-footer-1-js" defer></script>
-   <script type="text/javascript" src="./assest/js/minify-footer-fa3d8e1c.min.js"  id="min-footer-2-js" defer></script>
+     <script src="../../../assest/js/slider.js"></script>
+    <script src="../../../assest/js/nav.js"></script>
+     <script src="../../../assest/js/swiper.min.js"></script>
+     <script src="../../../assest/js/whatsapp.js"></script>
+    <script type="text/javascript" src="../../../assest/js/minify-footer-2ef81eaa.min.js"  id="min-footer-1-js" defer></script>
+   <script type="text/javascript" src="../../../assest/js/minify-footer-fa3d8e1c.min.js"  id="min-footer-2-js" defer></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
 </html>
